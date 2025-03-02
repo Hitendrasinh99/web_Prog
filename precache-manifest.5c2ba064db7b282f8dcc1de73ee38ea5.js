a6f387394f2175a45d78a08560e9a4928e3d14e3
Hitendrasinh Chavda <hitendrasinh99>
2025-03-02 02:02:49 -0500

    Updates

---
+++ precache-manifest.5c2ba064db7b282f8dcc1de73ee38ea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271fcb21c3b85e467ed1db51a72a7b84",
+    "url": "/web_Prog/index.html"
+  },
+  {
+    "revision": "c405e824c8a805828345",
+    "url": "/web_Prog/static/css/main.0f0f1189.chunk.css"
+  },
+  {
+    "revision": "39c49dadda0c8404879d",
+    "url": "/web_Prog/static/js/2.5ec05c3f.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/web_Prog/static/js/2.5ec05c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e6401820f2dcfdb768",
+    "url": "/web_Prog/static/js/3.01ae9db4.chunk.js"
+  },
+  {
+    "revision": "c405e824c8a805828345",
+    "url": "/web_Prog/static/js/main.ea51d39c.chunk.js"
+  },
+  {
+    "revision": "75f1d95a9593cc7d629a",
+    "url": "/web_Prog/static/js/runtime-main.8de68d24.js"
+  }
+]);
